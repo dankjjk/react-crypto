c9eb43a59f993c0210b8ff57a9d05fe1482a20a4
Alexander <dankjjk@gmail.com>
2022-08-24 16:42:41 +0900

    Second commit

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,39 +1,50 @@
 import React from 'react';
+import AssetsService from './axios';
 import './Form.scss';
-import Button from './Button/Button';
 
 const Form = () => {
-    const [formBtn, setFormBtn] = React.useState();
-
     const [giveBtn, setGiveBtn] = React.useState('');
-    const [getBtn, setGetBtn] = React.useState('');
-
-    const [giveBtnDirty, setGiveBtnDirty] = React.useState(false);
-    const [getBtnDirty, setGetBtnDirty] = React.useState(false);
-
-    const [giveBtnError, setGiveBtnError] = React.useState('Строка не может быть пустым');
-    const [getBtnError, setGetBtnError] = React.useState('Строка не может быть пустым');
 
     const giveHandler = (e) => {
         let text = e.target.value.replace(/\D/g, '');
         setGiveBtn(text);
     };
 
-    const blurHandler = (e) => {
-        switch (e.target.name) {
-            case 'give':
-                setGiveBtnDirty(true);
-                break;
-            case 'get':
-                setGetBtnDirty(true);
-                break;
-        }
+    // First dropdown
+
+    const dropdownRef = React.useRef(null);
+    const [isActive, setIsActive] = React.useState(false);
+    const onClick = (e) => {
+        e.preventDefault();
+        setIsActive(!isActive);
     };
 
-    // const onFormBtn = (btn) => {
-    //     btn.preventDefault();
-    //     console.log('he');
-    // };
+    // Second dropdown
+
+    const dropdownRefFiat = React.useRef(null);
+    const [isActiveFiat, setIsActiveFiat] = React.useState(false);
+    const onClickFiat = (e) => {
+        e.preventDefault();
+        setIsActiveFiat(!isActiveFiat);
+    };
+
+    // Axios
+
+    const assetsService = new AssetsService();
+
+    const [cryptoAssets, setCryptoAssets] = React.useState();
+    const [fiat, setFiat] = React.useState();
+
+    React.useEffect(() => {
+        assetsService.getCryptoAssets().then((result) => {
+            console.log(result);
+            setCryptoAssets(result);
+        });
+        assetsService.getNonCryptoAssets().then((result) => {
+            console.log(result);
+            setFiat(result);
+        });
+    }, []);
 
     return (
         <div className="form">
@@ -49,22 +60,64 @@ const Form = () => {
                             <div className="form-wrapper pl pr">
                                 <input
                                     value={giveBtn}
-                                    onBlur={(e) => blurHandler(e)}
                                     onChange={(e) => giveHandler(e)}
-                                    name="give"
                                     type="text"
                                     placeholder="Мин. 5000"></input>
                                 <div className="form-wrapper__currency">
                                     <span>RUB</span>
                                 </div>
                             </div>
-                            {giveBtnDirty && giveBtnError && (
-                                <div style={{ color: 'red' }}>{giveBtnError}</div>
-                            )}
                         </div>
 
                         <div className="form-right col-6">
-                            <Button onFormBtn />
+                            <div onClick={onClick} className="form-wrapper pl pr">
+                                <div className="form-wrapper__text">
+                                    <img src="./img/currency/qiwi.svg" alt="Qiwi" />
+                                    <span>QIWI</span>
+                                </div>
+                                <div className="form-wrapper__arrow">
+                                    <svg
+                                        width="17"
+                                        height="10"
+                                        viewBox="0 0 17 10"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M1 1.056 8.5 9 16 1"
+                                            stroke="#E23F65"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div
+                                ref={dropdownRef}
+                                className={`menu ${isActive ? 'active' : 'inactive'}`}>
+                                <ul>
+                                    {Array.isArray(cryptoAssets)
+                                        ? cryptoAssets.map((asset) => {
+                                              let id, icon, name;
+                                              if (asset.hasOwnProperty('address')) {
+                                                  //  Если крипта
+                                                  id = asset.asset.id;
+                                                  name = asset.asset.name;
+                                                  icon = asset.asset.icon;
+                                              } else {
+                                                  id = asset.id;
+                                                  name = asset.name;
+                                                  icon = asset.icon;
+                                              }
+                                              return (
+                                                  <li key={id}>
+                                                      <img src={icon} alt="" />
+                                                      <a href="/">{name}</a>
+                                                  </li>
+                                              );
+                                          })
+                                        : null}
+                                </ul>
+                            </div>
                         </div>
                     </div>
 
@@ -76,22 +129,14 @@ const Form = () => {
                     <div className="form-box form-get mb-2">
                         <div className="form-left col-6">
                             <div className="form-wrapper pl pr">
-                                <input
-                                    value={getBtn}
-                                    onBlur={blurHandler}
-                                    type="text"
-                                    name="get"
-                                    placeholder="Резерв:120.38"></input>
+                                <input type="text" name="get" placeholder="Резерв:120.38"></input>
                                 <div className="form-wrapper__currency">
                                     <span>BTC</span>
                                 </div>
                             </div>
-                            {getBtnDirty && getBtnError && (
-                                <div style={{ color: 'red' }}>{getBtnError}</div>
-                            )}
                         </div>
                         <div className="form-right col-6">
-                            <button className="form-wrapper pl pr">
+                            <div onClick={onClickFiat} className="form-wrapper pl pr">
                                 <div className="form-wrapper__text">
                                     <img src="./img/currency/bitcoin.svg" alt="Bitcoin" />
                                     <span>Bitcoin</span>
@@ -111,7 +156,34 @@ const Form = () => {
                                             strokeLinejoin="round"></path>
                                     </svg>
                                 </div>
-                            </button>
+                            </div>
+                            <div
+                                ref={dropdownRefFiat}
+                                className={`menu ${isActiveFiat ? 'active' : 'inactive'}`}>
+                                <ul>
+                                    {Array.isArray(fiat)
+                                        ? fiat.map((asset) => {
+                                              let id, icon, name;
+                                              if (asset.hasOwnProperty('address')) {
+                                                  //  Если крипта
+                                                  id = asset.asset.id;
+                                                  name = asset.asset.name;
+                                                  icon = asset.asset.icon;
+                                              } else {
+                                                  id = asset.id;
+                                                  name = asset.name;
+                                                  icon = asset.icon;
+                                              }
+                                              return (
+                                                  <li key={id}>
+                                                      <img src={icon} alt="" />
+                                                      <a href="/">{name}</a>
+                                                  </li>
+                                              );
+                                          })
+                                        : null}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
